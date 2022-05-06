ecfdfdeaf200bd0380e14142764c819d2fca1b85
ValerioKapa <58628293+ValerioKapa@users.noreply.github.com>
2022-05-06 16:15:47 +0300

    last version

--- app/src/main/java/com/example/day_tripv3/CustomerAdapter.java
+++ app/src/main/java/com/example/day_tripv3/CustomerAdapter.java
@@ -15,6 +15,7 @@ import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.orhanobut.dialogplus.DialogPlus;
@@ -51,19 +52,17 @@ public class CustomerAdapter extends FirestoreRecyclerAdapter<Customer, Customer
         holder.update_bttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogPlus dialogPlus = DialogPlus.newDialog(holder.textViewName.getContext())
-                        .setContentHolder(new ViewHolder(R.layout.update_dialog))
-                        .setExpanded(true, (int)(getScreenHeight() * 0.7f))
-                        .setCancelable(true)
-                        .create();
 
-                View view = dialogPlus.getHolderView();
+                BottomSheetDialog dialog = new BottomSheetDialog(holder.textViewName.getContext());
+                dialog.setContentView(R.layout.update_dialog);
+                dialog.setCanceledOnTouchOutside(true);
 
-                updateInfoName = view.findViewById(R.id.update_info_name_id);
-                updateInfoSurname = view.findViewById(R.id.update_info_surname_id);
-                updateInfoHotel = view.findViewById(R.id.update_info_hotel_id);
-                updateInfoPhone = view.findViewById(R.id.update_info_phone_id);
-                updateInfoButton = view.findViewById(R.id.update_info_button);
+                    //INSTEAD OF VIEW, USE DIALOG.
+                updateInfoName = dialog.findViewById(R.id.update_info_name_id);
+                updateInfoSurname = dialog.findViewById(R.id.update_info_surname_id);
+                updateInfoHotel = dialog.findViewById(R.id.update_info_hotel_id);
+                updateInfoPhone = dialog.findViewById(R.id.update_info_phone_id);
+                updateInfoButton = dialog.findViewById(R.id.update_info_button);
 
                 //THE ORDER OF THESE SHOUD BE THE SAME AS THE CONSTRUCTOR OF CUSTOMER
                 updateInfoName.setText(model.getName());
@@ -73,10 +72,10 @@ public class CustomerAdapter extends FirestoreRecyclerAdapter<Customer, Customer
                 ID = model.getId();
                 Trip = model.getTrip();
 
+                dialog.show();
+
                 //TODO remove
                 Toast.makeText(holder.textViewName.getContext(), "ID: "+ID+"  "+"Trip: "+Trip, Toast.LENGTH_LONG).show();
-
-                dialogPlus.show();
 
                 updateInfoButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -95,17 +94,18 @@ public class CustomerAdapter extends FirestoreRecyclerAdapter<Customer, Customer
                                     @Override
                                     public void onSuccess(Void unused) {
                                         Toast.makeText(holder.textViewName.getContext(), "Εκσυγχρόνιση Επιτυχής", Toast.LENGTH_LONG).show();
-                                         dialogPlus.dismiss();
+                                         //dialogPlus.dismiss();
                                     }
                                 }).addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(holder.textViewName.getContext(), "Δοκιμάστε Ξανά", Toast.LENGTH_LONG).show();
-                                dialogPlus.dismiss();
+                                //dialogPlus.dismiss();
                             }
                         });
                     }
                 });
+
             }
         });
     }
